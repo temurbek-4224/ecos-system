7f51419b23992280f2c4870238cb35d9fc80517c
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-10-27 16:46:42 +0500

    I have added new pictures to this project

--- src/data/data.js
+++ src/data/data.js
@@ -7,72 +7,72 @@ const DATA = [
   {
     id: 2,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2014/02/05/08/19/smoke-258786_960_720.jpg'
   },
   {
     id: 3,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2017/09/08/18/20/garbage-2729608_960_720.jpg'
   },
   {
     id: 4,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2017/02/26/16/51/cyclone-2100663_960_720.jpg'
   },
   {
     id: 5,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2018/03/25/09/15/world-3258865_960_720.jpg'
   },
   {
     id: 6,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2017/06/17/11/39/power-plant-2411932_960_720.jpg'
   },
   {
     id: 7,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2014/04/17/23/26/environmental-protection-326923_960_720.jpg'
   },
   {
     id: 8,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2020/05/21/19/09/away-5202207_960_720.jpg'
   },
   {
     id: 9,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://images.pexels.com/photos/1108572/pexels-photo-1108572.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
   },
   {
     id: 10,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2019/07/19/23/16/power-plant-4349830_960_720.jpg'
   },
   {
     id: 11,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2013/11/24/10/18/earth-216834_960_720.jpg'
   },
   {
     id: 12,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2018/02/03/13/04/refinery-3127588_960_720.jpg'
   },
   {
     id: 13,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2013/09/11/12/01/biological-181237_960_720.jpg'
   },
   {
     id: 14,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2016/10/22/21/54/industry-1761801_960_720.jpg'
   },
   {
     id: 15,
     name: 'Ekologik Muammolar',
-    imageUrl: 'https://media.gettyimages.com/photos/were-all-responsible-for-creating-a-better-tomorrow-picture-id1009934102?s=2048x2048'
+    imageUrl: 'https://cdn.pixabay.com/photo/2015/11/05/08/33/earth-1023859_960_720.jpg'
   },
 ]
 
