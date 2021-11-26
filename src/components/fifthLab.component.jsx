5b14515f65198a72cd8bf502e7cdcd3c01c20d71
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-11-26 22:53:00 +0500

    3- and 5- components are finished

--- src/components/fifthLab.component.jsx
+++ src/components/fifthLab.component.jsx
@@ -1,11 +1,60 @@
 import React from "react";
-
+import styled from "styled-components";
+import Title from "./littleComponents/title.component";
 const FifthLab = () => {
   return (
     <div>
-      5-Amaliyot
+      <Container>
+        <Title>Uchinchi amaliy ish</Title>
+        <h1>😇Bu amaliy mashg'ulat nazariy topshiriqlardan iborat.😇</h1>
+        <h2 style={{
+          marginTop: '20px',
+          fontSize: '48px',
+          textAlign: 'center'
+        }}>🧐🧐🧐</h2>
+      </Container>
     </div>
   )
 }
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: radial-gradient(
+    134.34% 134.34% at 50% 0%,
+    #1f3757 0%,
+    #131537 100%
+  );
+  color: #fff;
+  h1{
+    text-align: center;
+    @media screen and (max-width: 720px){
+      font-size: 18px;
+    }
+  }
 
+  form{
+    display: flex;
+    padding: 20px 40px;
+    width: 100%;
+    text-align: center;
+    align-items: center;
+    flex-direction: column;
+    div{
+      width: 80%;
+      display: flex;
+      justify-content: space-between;
+      @media screen and (max-width: 720px) {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      section{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    }
+  }
+`
 export default FifthLab;
--- src/components/thirdLab.component.jsx
+++ src/components/thirdLab.component.jsx
@@ -1,11 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+import Title from "./littleComponents/title.component";
 
 const ThirdLab = () => {
   return (
-    <div>
-      3-labaratoriya
-    </div>
+    <Container>
+      <Title>Uchinchi amaliy ish</Title>
+      <h1>😇Bu amaliy mashg'ulat nazariy topshiriqlardan iborat.😇</h1>
+      <h2 style={{
+        marginTop: '20px',
+        fontSize: '48px',
+        textAlign: 'center'
+      }}>🧐🧐🧐</h2>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: radial-gradient(
+    134.34% 134.34% at 50% 0%,
+    #1f3757 0%,
+    #131537 100%
+  );
+  color: #fff;
+  h1{
+    text-align: center;
+    @media screen and (max-width: 720px){
+      font-size: 18px;
+    }
+  }
+
+  form{
+    display: flex;
+    padding: 20px 40px;
+    width: 100%;
+    text-align: center;
+    align-items: center;
+    flex-direction: column;
+    div{
+      width: 80%;
+      display: flex;
+      justify-content: space-between;
+      @media screen and (max-width: 720px) {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      section{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    }
+  }
+`
 
 export default ThirdLab;
