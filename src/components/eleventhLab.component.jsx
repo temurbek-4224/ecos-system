eaea89e1c7efce91bfe0c11395ca8f206bab37d3
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-12-19 17:56:03 +0500

    I have added 11-lab to this projects

--- src/components/eleventhLab.component.jsx
+++ src/components/eleventhLab.component.jsx
@@ -1,19 +1,203 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Title from "./littleComponents/title.component";
-
+import CustomInput from './littleComponents/input.component';
+import CustomButton from "./littleComponents/customButton.component";
+const newInputValues = {
+  number: '',
+  massaOfYoq: '',
+  kulliligi: '',
+  iflosModdaMassasi: '',
+  oxidOfUglerod: '',
+  oxidAzod: '',
+  oxidQorgoshin: '',
+  k1: '',
+  k2: '',
+}
 const EleventhLab = () => {
+  const [inputValues, setInputValues] = useState(newInputValues);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const values = { ...inputValues };
+    values[name] = value;
+    setInputValues(values);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setInputValues(newInputValues);
+  }
   return (
     <Container>
       <Title>O'n Birinchi Amaliy mashg'ulotlar</Title>
 
-      <form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <section>
+            <CustomInput
+              type='text'
+              label='Variant nomeri'
+              name='number'
+              value={inputValues.number}
+              handleChange={handleChange}
+            />
+            <CustomInput
+              type='text'
+              label="Yoqilgan massasi"
+              name='massaOfYoq'
+              value={inputValues.massaOfYoq}
+              handleChange={handleChange}
+            />
+            <CustomInput
+              type='text'
+              label="Yoqilg'ining kulliligi"
+              name='kulliligi'
+              value={inputValues.kulliligi}
+              handleChange={handleChange}
+            />
+          </section>
+          <section>
+            <CustomInput
+              type='text'
+              label="Iflos modda massasi"
+              name='iflosModdaMassasi'
+              value={inputValues.iflosModdaMassasi}
+              handleChange={handleChange}
+            />
+            <CustomInput
+              type='text'
+              label="Uglerod oxidi"
+              name='oxidOfUglerod'
+              value={inputValues.oxidOfUglerod}
+              handleChange={handleChange}
+            />
+            <CustomInput
+              type='text'
+              label="Azot oxidlari"
+              name='oxidAzod'
+              value={inputValues.oxidAzod}
+              handleChange={handleChange}
+            />
+          </section>
+          <section>
+            <CustomInput
+              type='text'
+              label="Qo'rg'oshin oxidlari"
+              name='oxidQorgoshin'
+              value={inputValues.oxidQorgoshin}
+              handleChange={handleChange}
+            />
+            <CustomInput
+              type='text'
+              label="k1 koefitsent"
+              name='k1'
+              value={inputValues.k1}
+              handleChange={handleChange}
+            />
+            <CustomInput
+              type='text'
+              label="k2 koefitsent"
+              name='k2'
+              value={inputValues.k2}
+              handleChange={handleChange}
+            />
+          </section>
+        </div>
 
+        <CustomButton type='submit'>Yechim</CustomButton>
       </form>
 
       <TableContainer>
-        <LargeTable></LargeTable>
-        <SmallTable></SmallTable>
+        <LargeTable>
+          <table className="table table-dark table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Variant nomeri</th>
+                <th scope="col">Yoqilgan massasi</th>
+                <th scope="col">Kulliligi</th>
+                <th scope="col">Iflos modda massasi</th>
+                <th scope="col">Uglerod oxidi</th>
+                <th scope="col">Azot oxidi</th>
+                <th scope="col">Qo'rg'oshin oxidi</th>
+                <th scope="col">k1</th>
+                <th scope="col">k2</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {
+                  Object.keys(inputValues).map((value, index) => {
+                    return (
+                      <td key={index}>{inputValues[value]}</td>
+                    );
+                  })
+                }
+              </tr>
+            </tbody>
+          </table>
+        </LargeTable>
+        <SmallTable>
+          <table className="table table-danger table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Variant nomeri</th>
+                <th scope="col">Yoqilgan massasi</th>
+                <th scope="col">Kulliligi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {
+                  Object.keys(inputValues).filter((value, index) => index < 3).map((value, index) => {
+                    return (
+                      <td key={index}>{inputValues[value]}</td>
+                    )
+                  })
+                }
+              </tr>
+            </tbody>
+          </table>
+          <table className="table table-danger table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Iflos modda massasi</th>
+                <th scope="col">Uglerod oxidi</th>
+                <th scope="col">Azot oxidi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {
+                  Object.keys(inputValues).filter((value, index) => index > 2 && index < 6).map((value, index) => {
+                    return (
+                      <td key={index}>{inputValues[value]}</td>
+                    )
+                  })
+                }
+              </tr>
+            </tbody>
+          </table>
+          <table className="table table-danger table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Qo'rg'oshin oxidi</th>
+                <th scope="col">k1</th>
+                <th scope="col">k2</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {
+                  Object.keys(inputValues).filter((value, index) => index > 5 && index < 9).map((value, index) => {
+                    return (
+                      <td key={index}>{inputValues[value]}</td>
+                    )
+                  })
+                }
+              </tr>
+            </tbody>
+          </table>
+        </SmallTable>
       </TableContainer>
 
       <ResultContainer>
